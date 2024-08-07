ae5494a5f83b89364e540e119a35e737deb4ee61
Jordi Guiusseppe Martinez Valencia <166647357+JordiBeppe@users.noreply.github.com>
2024-08-07 01:41:48 +0000

    Methods added

--- src/app.py
+++ src/app.py
@@ -8,8 +8,9 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, People, Favorite, Planet
+# import requests
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -36,14 +37,159 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+    #lineas user
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_users():
+    user = User()
+    user = user.query.all()
+    user = list(map(lambda item: item.serialize(), user))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(user), 200
 
-    return jsonify(response_body), 200
+
+
+    #lineas People
+
+@app.route("/people", methods=["GET"])
+def get_all_people():
+
+    people = People()
+    people = people.query.all()
+    people = list(map(lambda item: item.serialize(), people))
+
+    return jsonify(people), 200
+
+@app.route("/people/<int:people_id>", methods=["GET"])
+def get_one_people(people_id):
+    people = People()
+    people = people.query.get(people_id)
+    
+    if people is None:
+        raise APIException("People not found", status_code=404)
+    else:
+        return jsonify(people.serialize()), 200
+    
+     #lineas Planet
+
+@app.route("/planet", methods=["GET"])
+def get_all_planet():
+
+    planet = Planet()
+    planet = planet.query.all()
+    planet = list(map(lambda item: item.serialize(), planet))
+
+    return jsonify(planet), 200
+
+@app.route("/planet/<int:planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    planet = Planet()
+    planet = planet.query.get(planet_id)
+    
+    if planet is None:
+        raise APIException("Planet not found", status_code=404)
+    else:
+        return jsonify(planet.serialize()), 200
+
+@app.route("/favorite/people/<int:people_id>", methods=["POST"])
+def add_people_fav(people_id):
+    user_id = 1
+
+    fav = Favorite()
+    fav.user_id = user_id
+    fav.people_id = people_id
+    
+    db.session.add(fav)
+
+    try:
+        db.session.commit()
+        return jsonify("success"), 201
+    except Exception as error:
+        db.session.rollback
+        return jsonify("try again"), 201
+    
+    
+@app.route("/favorite/people/<int:people_id>", methods=["DELETE"])
+def delete_people_fav(people_id):
+    user_id = 1
+
+    fav = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+    
+    if fav:
+        db.session.delete(fav)
+        try:
+            db.session.commit()
+            return jsonify("se eliminó exitosamente"), 200
+        except Exception as error:
+            db.session.rollback()
+            return jsonify({"error": str(error)}), 500
+    else:
+        return jsonify("favorito no encontrado"), 404
+    
+    #----------------
+    
+
+@app.route("/favorite/planet/<int:planet_id>", methods=["POST"])
+def add_planet_fav(planet_id):
+    user_id = 1
+
+    fav = Favorite()
+    fav.user_id = user_id
+    fav.planet_id = planet_id
+    
+    db.session.add(fav)
+
+    try:
+        db.session.commit()
+        return jsonify("success"), 201
+    except Exception as error:
+        db.session.rollback
+        return jsonify("try again"), 201
+    
+    
+@app.route("/favorite/planet/<int:planet_id>", methods=["DELETE"])
+def delete_planet_fav(planet_id):
+    user_id = 1
+
+    fav = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    
+    if fav:
+        db.session.delete(fav)
+        try:
+            db.session.commit()
+            return jsonify("se eliminó exitosamente"), 200
+        except Exception as error:
+            db.session.rollback()
+            return jsonify({"error": str(error)}), 500
+    else:
+        return jsonify("favorito no encontrado"), 404
+    
+
+
+
+
+# @app.route("/people/population", methods=["GET"])
+# def get_people_population():
+#     response = requests.get("https://www.swapi.tech/api/people?page=1&limit=3")
+#     response = response.json()
+#     response = response.get("results")
+
+#     for item in response:
+#         result = requests.get(item.get("url")).json().get("result").get("properties")
+#         people = People()
+#         attributes = ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender"]
+        
+#         for attr in attributes:
+#             setattr(people, attr, result.get(attr))
+#         db.session.add(people)
+
+#     try:
+#         db.session.commit()
+#         return jsonify("populando listo"), 200
+#     except Exception as error:
+#         print(error)
+#         db.session.rollback()
+#         return jsonify("error"), 500
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
